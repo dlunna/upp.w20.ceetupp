ccaca5609a23ae44e5336c307651e525209850b1
dlunna <dlunna@gmail.com>
2020-04-09 23:27:25 -0500

    Solo texto covi

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <?php include ('./dotk/00head/analitycs.html'); ?>
+  <?php include ('./dotk/00head/cabecera.html'); ?>
+  <?php include ('./dotk/00head/estilos.html'); ?>
+</head>
+
+<body>
+
+  <a id="arriba"></a>
+  <header>
+    <!-- Logos e imagen de hidalgo -->
+    <?php include ('../lib18/seph/hf/header.html'); ?>
+
+    <?php //include ('./dotk/01header/carousel-principal/carouselimg.php'); ?>
+
+  </header>
+
+  <?php include ('./dotk/01header/02begajoso/menu-pegajoso.html'); ?>
+
+  <main>
+    <div class="container marketing">
+
+      <br>
+      <br>
+
+      <h1>Centro Ecuestre y de Equinoterapia de la Universidad Politécnica de Pachuca</h1>
+      <p>El CEETUPP es un Proyecto Social sin fines de lucro, cuyos objetivos son ofrecer terapia para la rehabilitación o habilitación por medio del caballo con apoyo de áreas médica, psicológica y pedagógica a través de personal especializado.</p>
+      <p>El rescate y fomento de las actividades hípicas que forman parte de nuestra cultura y de nuestra identidad como mexicanos. El Centro Ecuestre y de Equinoterapia de la Universidad Politécnica de Pachuca inicia sus labores en el 2005. </p>
+
+<!--
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <a href="./images/xint1.jpg">
+              <img src="./images/xint1.jpg" alt="Estancia de Investigación por parte de la Ingeniería en Software en Osaka, Japón" class="img-fluid rounded">
+            </a>
+          </div>
+          <div class="col-sm">
+            <a href="./images/xint3.jpg">
+              <img src="./images/xint3.jpg" alt="Estudiantes de la UPP en empresa Agronomia en Valpaíso, Chile" class="img-fluid rounded">
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <br>
+      <br>
+-->
+      <h1>Equinoterapia</h1>
+
+      <p>La equinoterapia brinda habilitación o rehabilitación mediante el caballo dentro de un enfoque interdisciplinario, en las áreas de salud, educación y equitación, buscando el desenvolvimiento biopsicosocial de personas con deficiencias físicas, psicológicas, educacionales y sociales.</p>
+
+      <p>Se brindan dos veces al año cursos de Equinoterapia en los meses de junio y octubre, con los cuales se han capacitado a más de 200 personas entre las que se encontraba personal de 4 Centros de Equinoterapia municipales así como 12 centros particulares en el país, que se abrieron y pusieron en función con las capacitaciones y asesorías del CEETUPP. </p>
+
+      <p><strong>Lunes a viernes de las 9:00 a las 14:00 horas<br>
+        Costo: $32.00 por sesión</strong> </p>
+
+<!--
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <a href="./images/xint2.jpg">
+              <img src="./images/xint2.jpg" alt="Estudiantes de Ingeniería Financiera, rumbo a la Universidad de Santander en Colombia" class="img-fluid rounded">
+            </a>
+          </div>
+          <div class="col-sm">
+            <a href="./images/xint4.jpg">
+              <img src="./images/xint4.jpg" alt="Estudiantes de la UPP, particantes de la convocatoria Grupo UNIS, en la ciudad de Varghinia, Brasil" class="img-fluid rounded">
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <br>
+      <br>
+-->
+      <h1>Charrería</h1>
+
+      <p>Arte considerado como Patrimonio Cultural Inmaterial de la Humanidad. La Universidad Politécnica de Pachuca a través de su Centro Ecuestre y de Equinoterapia otorga a los interesados la Escuela de Charrería para mejorar las técnicas de las suertes charras, sumado al orgullo de contribuir a mantener  vivas nuestras tradiciones, la identidad como mexicanos y aportar beneficios a la gente de la región y del estado que practique la disciplina.</p>
+
+      <p>Somos una Universidad que fomenta el deporte de la Charrería de manera correcta desde el inicio (vestimenta del charro y arreos del caballo), monta, floreo de reata y suertes de la Charrería, así como la primera en formar binomios y equipos competentes en las diferentes disciplinas de la misma.</p>
+
+      <p><strong>Lunes a viernes de las 15:30 a 18:30 horas<br>
+        Costo: $75.00 por clase, público en general</strong></p>
+
+<!--
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <a href="./images/xint5.jpg">
+              <img src="./images/xint5.jpg" alt="Movilida académica internacional en Grupo Unis, en Varghinia, Brasil" class="img-fluid rounded">
+            </a>
+          </div>
+          <div class="col-sm">
+            <iframe width="100%" height="315" class="embed-responsive-item" src="https://www.youtube.com/embed/zILtmXy5wlE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+
+      <br>
+      <br>
+-->
+
+
+
+
+
+
+
+
+
+
+      <br />
+      <br />
+      <a id="mapadesitio"></a>
+      <hr class="featurette-divider">
+      <!-- Menu con todos los sitios y mini sitios de la página -->
+      <?php include ('../lib18/coreFRONTx/main/tmenu-footer/menufooter.php'); ?>
+
+    </div>
+
+  </main>
+
+  <footer>
+    <!-- footer de gobierno del estado -->
+    <?php include ('../lib18/seph/hf/footer.html'); ?>
+  </footer>
+
+  <?php include ('../lib18/coreFRONTx/09js/java.html'); ?>
+
+  <!-- Script para dar la animacion al boton flotante que lleva arriba -->
+  <a href="#" class="back-to-top">Volver arriba</a>
+  <script src="../lib18/coreFRONTx/10boton-arriba/script.js"></script>
+
+</body>
+
+</html>
